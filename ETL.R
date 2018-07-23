adeb2305014b5761cec5c22fdab610206dfd7aca
Ryan Cushen <cushenr@gmail.com>
2018-07-23 18:38:45 +1000

    Initial commit

---
+++ ETL.R
@@ -0,0 +1,33 @@
+library(tidyr)
+library(readxl)
+
+# Import and clean data
+
+datafile <- 'data'
+raw_data <- read_excel(datafile)
+
+cleaned_data <- raw_data %>%
+  
+
+
+# Specify desired banks and variables
+
+banks <- c(
+  '',
+  '',
+  ''
+)
+
+vars <- c(
+  '',
+  '',
+  ''
+)
+
+# Select and output the relevant data
+
+selected_data <- cleaned_data %>%
+  select(vars) %>%
+  filter(WVB %in% banks)
+
+write_csv(selected_data, 'output/selected_data.csv')
